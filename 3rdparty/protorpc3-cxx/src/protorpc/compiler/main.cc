4fc48f9fcc185559d6b73ec82bf4abe1138f5d72
chai2010 <chaishushan@gmail.com>
2016-03-04 16:58:37 +0800

    3rdparty: remove protorpc3-cxx

--- 3rdparty/protorpc3-cxx/src/protorpc/compiler/main.cc
+++
@@ -1,9 +0,0 @@
-// Copyright 2016 <chaishushan{AT}gmail.com>. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-#include "protorpc/compiler/protorpc_plugin.h"
-
-int main(int argc, char *argv[]) {
-	return protorpc_plugin_Main(argc, argv);
-}
